1cee03e8b1c6c5f3ffb86edeb1025332c6e48263
robMolloy <robmolloy@hotmail.co.uk>
2024-06-06 19:03:28 +0100

    v0.0 - it adds up

---
+++ src/components/Inputs.tsx
@@ -0,0 +1,64 @@
+import { FormEvent } from "react";
+
+export function TextInput(p: {
+  onInput: (x: string) => void;
+  value: string;
+  placeholder: string;
+  label: React.ReactNode;
+  error: string;
+}) {
+  return (
+    <div>
+      <label className="label">
+        <span className="label-text">{p.label}</span>
+      </label>
+      <input
+        type="text"
+        placeholder={p.placeholder}
+        className="input input-bordered w-full bg-white text-slate-600"
+        onInput={(e) => {
+          const e2 = e as FormEvent<HTMLInputElement> & { target: { value: string } };
+          const value = e2.target.value;
+          p.onInput(value);
+        }}
+        value={p.value}
+      />
+      <div className="label">
+        <span className={`badge badge-error w-full ${p.error ? "" : "opacity-0"}`}>{p.error}</span>
+      </div>
+    </div>
+  );
+}
+export function NumberInput(p: {
+  onInput: (x: number) => void;
+  value: number;
+  placeholder: string;
+  label: React.ReactNode;
+  error?: string;
+}) {
+  return (
+    <div>
+      <label className="label">
+        <span className="label-text">{p.label}</span>
+      </label>
+      <input
+        type="text"
+        placeholder={p.placeholder}
+        className="input input-bordered w-full bg-white text-slate-600"
+        onInput={(e) => {
+          const e2 = e as FormEvent<HTMLInputElement> & { target: { value: string } };
+          const value = e2.target.value;
+          p.onInput(Number(value));
+        }}
+        value={p.value}
+      />
+      {p.error && (
+        <div className="label">
+          <span className={`badge badge-error w-full ${p.error ? "" : "opacity-0"}`}>
+            {p.error}
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,5 +1,123 @@
+import { NumberInput, TextInput } from "@/components";
 import { Typography } from "@/components/Typography";
+import { useState } from "react";
 
 export default function Home() {
-  return <Typography fullPage>So much set up</Typography>;
+  const [formData, setFormData] = useState({
+    width: 10,
+    length: 10,
+    excavationDepth: 0.2,
+    fillDepth: 0.3,
+  });
+  return (
+    <Typography fullPage>
+      <h1>Patio Calculator</h1>
+
+      <NumberInput
+        label="width"
+        placeholder="placeholder"
+        onInput={(x) => setFormData({ ...formData, width: x })}
+        value={formData.width}
+        error=""
+      />
+      <NumberInput
+        label="length"
+        placeholder="placeholder"
+        onInput={(x) => setFormData({ ...formData, length: x })}
+        value={formData.length}
+        error=""
+      />
+      <NumberInput
+        label="excavation depth"
+        placeholder="placeholder"
+        onInput={(x) => setFormData({ ...formData, excavationDepth: x })}
+        value={formData.excavationDepth}
+        error=""
+      />
+      <NumberInput
+        label="fill depth"
+        placeholder="placeholder"
+        onInput={(x) => setFormData({ ...formData, fillDepth: x })}
+        value={formData.fillDepth}
+        error=""
+      />
+
+      <div className="flex">
+        <div className="flex-1">
+          <h2>Amounts</h2>
+
+          <div className="flex items-baseline gap-4">
+            <div
+              className="tooltip"
+              data-tip="formData.width * formData.length * formData.excavationDepth"
+            >
+              <button className="btn btn-circle btn-info">i</button>
+            </div>
+            <p className="text-xl">
+              Excavation: {formData.width * formData.length * formData.excavationDepth} m^3
+            </p>
+          </div>
+
+          <div className="flex items-baseline gap-4">
+            <div
+              className="tooltip"
+              data-tip="formData.width * formData.length * formData.fillDepth"
+            >
+              <button className="btn btn-circle btn-info">i</button>
+            </div>
+            <p className="text-xl">
+              Fill: {formData.width * formData.length * formData.fillDepth} m^3
+            </p>
+          </div>
+
+          <div className="flex items-baseline gap-4">
+            <div className="tooltip" data-tip="formData.width * formData.length">
+              <button className="btn btn-circle btn-info">i</button>
+            </div>
+            <p className="text-xl">Stone: {formData.width * formData.length} m^2</p>
+          </div>
+        </div>
+        <div className="flex-1">
+          <h2>Costs</h2>
+
+          <div className="flex items-baseline gap-4">
+            <div
+              className="tooltip"
+              data-tip="formData.width * formData.length * formData.excavationDepth * 50"
+            >
+              <button className="btn btn-circle btn-info">i</button>
+            </div>
+            <p className="text-xl">
+              Excavation: £{formData.width * formData.length * formData.excavationDepth * 50}
+            </p>
+          </div>
+
+          <div className="flex items-baseline gap-4">
+            <div
+              className="tooltip"
+              data-tip="formData.width * formData.length * formData.fillDepth * 100"
+            >
+              <button className="btn btn-circle btn-info">i</button>
+            </div>
+            <p className="text-xl">
+              Fill: £{formData.width * formData.length * formData.fillDepth * 100}
+            </p>
+          </div>
+
+          <div className="flex items-baseline gap-4">
+            <div className="tooltip" data-tip="formData.width * formData.length * 20">
+              <button className="btn btn-circle btn-info">i</button>
+            </div>
+            <p className="text-xl">Stone: £{formData.width * formData.length * 20} </p>
+          </div>
+        </div>
+      </div>
+      <h2>
+        Total: £
+        {formData.width * formData.length * formData.excavationDepth * 50 +
+          formData.width * formData.length * formData.fillDepth * 100 +
+          formData.width * formData.length * 20}
+      </h2>
+    </Typography>
+  );
 }
